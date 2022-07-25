3948eb46451d748aee005d3980e8c6ce70190f2d
boyerp23 <82129609+boyerp23@users.noreply.github.com>
2022-07-24 19:02:46 -0500

    Add files via upload

---
+++ My 1st python application.py
@@ -0,0 +1,22 @@
+# File creator: Paul Boyer
+#pauboyer@uat.edu
+#CSC235 Python Programming 1
+# WK 1 assignment 1
+
+# Information that we will call to print out
+intro = "Appropriate use of mayonnaise\n"+\
+    "This is to demonstrate where mayonnaise belongs should you find it in your hosehold \n\n"
+#Steps for completing the program instruction
+s1 ='a'
+s1 = f's{1} Search for mayonnaise within household\n'
+s2 = f's{2} Locate the nearest garbage can \n'
+s3 = f's{3} Take mayonnaise to garbage can\n'
+s4 = f's{4} Throw it into the garbage with gusto because that\'s where it belongs\n'
+s5 = f's{5} Have no remorse for throwing it away even if others like it\n'
+# calling the print function to display information 
+print(intro)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+print(s5)
